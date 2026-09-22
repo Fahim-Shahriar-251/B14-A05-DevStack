b2760ffbf9fa5e3c740f9493b8b5bbc2623ae391
Fahim Shahriar <fahimfj72@gmail.com>
2026-09-22 21:08:39 +0600

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h2 className='text-red-400'>Welcome to hell</h2>
+      <button className="btn btn-neutral">Neutral</button>
+    </>
+  )
+}
+
+export default App;
