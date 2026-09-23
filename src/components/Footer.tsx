02224698adace72beb394276681beb88c9991cc7
Fahim Shahriar <fahimfj72@gmail.com>
2026-09-23 17:00:33 +0600

    Make responsive footer for small device

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <hr className="text-gray-300" />
 
             <div className="container mx-auto">
-                <div className="flex justify-between pt-1 pl-2.5 pr-2.5 mb-10 gap-3">
+                <div className="flex flex-col md:flex justify-between pt-1 pl-2.5 pr-2.5 mb-10 gap-3">
                     <div className="space-y-3.5">
                         <img className="h-6.25 w-auto" src={logo} alt="Logo" />
                         <p className="text-gray-500">Curated tools, technologies, and resources for developers building
@@ -45,7 +45,7 @@ const Footer = () => {
 
                 <hr className="text-gray-300 container mx-auto" />
 
-                <div className="flex justify-between  pl-2.5 pr-2.5 mt-10">
+                <div className="flex flex-col md:flex justify-between  pl-2.5 pr-2.5 mt-10">
                     <h2 className="text-gray-500">© 2026 Dev Stack. All rights reserved.</h2>
 
                     <ul className="flex justify-between text-gray-500 gap-5">
